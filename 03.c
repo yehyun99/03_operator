d3f1d573a94a96c159c36b6270e1f5ec5d2fe118
이예현 <shszyh@iclooud.com>
2018-10-02 15:47:20 +0900

    02

--- 03.c
+++ 03.c
@@ -4,18 +4,11 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a,b,i;
-	printf("input two integers:");
-	scanf("%d %d",&a,&b);
-	i=a+b;
-	printf("+ result is %d\n",i);
-	i=a-b;
-	printf("- result is %d\n",i);
-	i=a*b;
-	printf("* result is %d\n",i);
-	i=a/b;
-	printf("/ result is %d\n",i);
-	i=a%b;
-	printf("%% result is %d\n",i);
+	int sec;
+	printf("input the second:");
+	scanf("%d",&sec);
+	
+	printf("%d the time is %i:%i\n",sec,sec/60,sec%60);
+	
 	return 0;
 }
