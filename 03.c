2271942ea9767c96cf404c350a4a6959a37fb5ab
이예현 <shszyh@iclooud.com>
2018-10-02 16:00:56 +0900

    03

--- 03.c
+++ 03.c
@@ -4,11 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sec;
-	printf("input the second:");
-	scanf("%d",&sec);
+	int y;
+	int result;
+	printf("input the year:");
+	scanf("%d",&y);
 	
-	printf("%d the time is %i:%i\n",sec,sec/60,sec%60);
+	
+	result=(y%4==0&&y%100!=0)||(y%400==0);
+	printf("is the year %d the leap year?:%d",y,result);
+	
 	
 	return 0;
 }
